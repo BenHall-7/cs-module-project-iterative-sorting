6d937dfcf759cdf0040800fe0ec640f1e9c04ec7
BenHall-7 <benjaminjahall@gmail.com>
2020-09-30 22:47:25 -0400

    Implement linear and binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,33 @@
 def linear_search(arr, target):
-    # Your code here
-
+    for i, v in enumerate(arr):
+        if v == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1
+    
+    l = 0
+    r = len(arr) - 1
 
-    # Your code here
+    if arr[l] > target or arr[r] < target:
+        return -1
 
+    if arr[l] == target:
+        return l
+    elif arr[r] == target:
+        return r
 
-    return -1  # not found
+    while r - l >= 2:
+        mid = (l + r) // 2
+        if arr[mid] > target:
+            r = mid
+        elif arr[mid] < target:
+            l = mid
+        else:
+            return mid
+    return -1
